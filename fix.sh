325914e32529691485c960819955287488cb7ca7
Vasiliy Kevroletin <kevroletin@gmail.com>
2016-12-07 15:14:09 +1000

    Remove background colors similar to shell themes
    Problem: most shell utils which I use (zsh, ranger, etc) use background
    colors to render font. This makes text unreadable because backgroud
    colors have similar brighness.
    
    Fix: similar themes in base16-shell repository configures background
    colors to same values as foreground colors. This works for me.

---
+++ fix.sh
@@ -0,0 +1,9 @@
+#
+# Make xresources theme similar to shell themes
+#
+sed -i 's/*.color9:       base09/*.color9:       base08/g' xresources/*
+sed -i 's/*.color10:      base01/*.color10:      base0B/g' xresources/*
+sed -i 's/*.color11:      base02/*.color11:      base0A/g' xresources/*
+sed -i 's/*.color12:      base04/*.color12:      base0D/g' xresources/*
+sed -i 's/*.color13:      base06/*.color13:      base0E/g' xresources/*
+sed -i 's/*.color14:      base0F/*.color14:      base0C/g' xresources/*
